9a9bcdedbad461be603d20c9cd5866d6742aaf87
Matistjati <matistjati@outlook.com>
2022-09-27 02:01:59 +0200

    Updated modmul to work for msvc

--- Competitive Programming/cpp/number theory/modMul.cpp
+++ Competitive Programming/cpp/number theory/modMul.cpp
@@ -1,7 +1,23 @@
+#if _LOCAL
+const int bits = 1;
+// if all numbers are less than 2^k, set bits = 64-k
+const ull po = 1ULL << bits;
+ull modmul(ull a, ull b, ull c) {
+    ull x = a * (b & (po - 1)) % c;
+    while ((b >>= bits) > 0) {
+        a = (a << bits) % c;
+        x += (a * (b & (po - 1))) % c;
+        x %= c;
+    }
+    return x % c;
+}
+
+#else
 ull modmul(ull a, ull b, ull M) {
     ll ret = a * b - M * ull(1.L / M * a * b);
     return ret + M * (ret < 0) - M * (ret >= (ll)M);
 }
+#endif
 ull modpow(ull b, ull e, ull mod) {
     ull ans = 1;
     for (; e; b = modmul(b, b, mod), e /= 2)
